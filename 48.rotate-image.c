2692294e1216b9497fa9d6e6b17c9a3cd1377f96
Miguel <miguel-meierbluewin.ch>
2023-06-24 14:39:09 +0200

    new

---
+++ 48.rotate-image.c
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=48 lang=c
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+
+void rotate(int **matrix, int matrixSize, int *matrixColSize)
+{
+
+    // initialize right and lefft spot
+    int l = 0;
+    int r = matrixSize - 1;
+
+    // run untill l<r
+    while (l < r)
+    {
+        for (int i = 0; i < r - l; i++) // run loop as many times as the size of the colum or row -1
+        {
+            // top is equal to l because we have a square
+            int top = l;
+            int bottom = r;
+
+            // saver top left
+            int temp1 = matrix[top][l + i];
+
+            // move bottom l to top l
+            matrix[top][l + i] = matrix[bottom - i][l];
+
+            // move bottom r to bottom l
+            matrix[bottom - i][l] = matrix[bottom][r - i];
+
+            // move top right to bottom right
+            matrix[bottom][r - i] = matrix[top + i][r];
+
+            // mover top l to top r
+            matrix[top + i][r] = temp1;
+        }
+
+        //
+        r--;
+        l++;
+    }
+}
+// @lc code=end
