ed122ba077ced7f7b5714b4f03d5eeb0abcadbd1
Miguel <miguel-meierbluewin.ch>
2023-06-25 17:26:41 +0200

    new

---
+++ 14.longest-common-prefix.c
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=14 lang=c
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+char *longestCommonPrefix(char **strs, int strsSize)
+{
+    //
+}
+// @lc code=end
