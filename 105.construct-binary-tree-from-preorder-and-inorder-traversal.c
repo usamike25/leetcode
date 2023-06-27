d2405703131121d4fb82f51f368841d1609ded17
Miguel <miguel-meierbluewin.ch>
2023-06-27 10:23:43 +0200

    new

--- 105.construct-binary-tree-from-preorder-and-inorder-traversal.c
+++ 105.construct-binary-tree-from-preorder-and-inorder-traversal.c
@@ -5,14 +5,12 @@
  */
 
 // @lc code=start
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 struct TreeNode* buildTree(int* preorder, int preorderSize, int* inorder, int inorderSize){
 
 }
