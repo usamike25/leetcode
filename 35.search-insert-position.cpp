8726d8c24d0d2e7bbcac1e77bbac232f4b1614f3
Miguel <miguel-meierbluewin.ch>
2023-11-02 13:36:39 +0100

    new

---
+++ 35.search-insert-position.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        // binary search
+
+        int r = nums.size() - 1;
+        int l = 0;
+        int m;
+
+        while (r > l)
+        {
+            m = l + (r - l) / 2;
+            if (nums[m] >= target)
+            {
+                r = m;
+            }
+            else
+            {
+                l = m + 1;
+            }
+        }
+        if (nums[r] == target)
+        {
+            return r;
+        }
+        else if (nums[r] < target)
+        {
+            return r + 1;
+        }
+        else
+        {
+            return r;
+        }
+    }
+};
+// @lc code=end
