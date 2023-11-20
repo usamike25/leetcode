fa4a7e4133340cb35470dd88d7a43e0a51c462ba
Miguel <miguel-meierbluewin.ch>
2023-11-20 12:45:47 +0100

    new

---
+++ 1984.minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1984 lang=cpp
+ *
+ * [1984] Minimum Difference Between Highest and Lowest of K Scores
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
