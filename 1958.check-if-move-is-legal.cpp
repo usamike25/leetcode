084a44bbced8b5b53be57e879916faf8edfaad04
Miguel <miguel-meierbluewin.ch>
2024-02-06 12:07:51 +0100

    new

---
+++ 1958.check-if-move-is-legal.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1958 lang=cpp
+ *
+ * [1958] Check if Move is Legal
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool checkMove(vector<vector<char>>& board, int rMove, int cMove, char color) {
+        
+    }
+};
+// @lc code=end
+
