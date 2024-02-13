40c9dae61973bf8fcb4dd1fa477020540a2a5208
Miguel <miguel-meierbluewin.ch>
2024-02-13 14:23:30 +0100

    new

---
+++ 120.triangle.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=120 lang=cpp
+ *
+ * [120] Triangle
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        if (triangle.size() == 1)
+        {
+            return triangle[0][0];
+        }
+
+        for (int i = triangle.size() - 2; i >= 0; i--)
+        {
+            for (int j = 0; j < triangle[i].size(); j++)
+            {
+                triangle[i][j] = min(triangle[i][j] + triangle[i + 1][j], triangle[i][j] + triangle[i + 1][j + 1]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
+// @lc code=end
