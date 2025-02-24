3e470734284a4511d2ccd188343074714b27a9ad
usamike25 <miguel-meier@bluewin.ch>
2025-02-24 11:16:10 +0800

    new

--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -6,35 +6,42 @@
 
 # @lc code=start
 class Solution:
-    solution = []
-
-    def combinationSum_rec(self, candidates: List[int], target: int, i, current_solution):
-
-        #basecases
-        current_sum = sum(current_solution)
-        if current_sum == target:
-            self.solution.append(list(current_solution))
-            return
-        if current_sum > target or i >= len(candidates):
-            return
-        
-
-        #recursive call
-
-        #add i #i is not incremented here!
-        
-        self.combinationSum_rec(candidates, target, i, current_solution + [candidates[i]])
-        # do not add i
-        self.combinationSum_rec(candidates, target, i+1, current_solution)
-
-
+   
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
-        self.solution = []  # Reset the solution list for each call
-        self.combinationSum_rec(candidates, target, 0, [])
+        return_list = []
+        curr_sol = []
+
+        def backtrack(index, cur_sum):
+            # basecase
+            if cur_sum > target:
+                return
+            if cur_sum == target:
+                return_list.append(curr_sol[:])
+                return
+            if index >= len(candidates):
+                return
+            
+            # lets add the left solution excluding the current index value
+            backtrack(index+1, cur_sum)
+
+            # append current index to solution set
+            curr_sol.append(candidates[index])
+
+            # use current index with allready given curr_sol
+            backtrack(index, cur_sum + candidates[index])
+
+            # delete from the right, last adde dvalue
+            curr_sol.pop()
+        
+        backtrack(0,0)
+        return return_list
+            
+
+            
 
 
-        return self.solution
+
 # @lc code=end
 
