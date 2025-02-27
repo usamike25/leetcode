16c99c3625c18c67a6ca049bea2c6d50e5c23cb7
usamike25 <miguel-meier@bluewin.ch>
2025-02-27 18:09:26 +0800

    new

--- 39.combination-sum.py
+++ 39.combination-sum.py
@@ -9,35 +9,31 @@ class Solution:
    
 
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-
-        return_list = []
-        curr_sol = []
-
-        def backtrack(index, cur_sum):
-            # basecase
-            if cur_sum > target:
-                return
-            if cur_sum == target:
-                return_list.append(curr_sol[:])
-                return
-            if index >= len(candidates):
-                return
-            
-            # lets add the left solution excluding the current index value
-            backtrack(index+1, cur_sum)
-
-            # append current index to solution set
-            curr_sol.append(candidates[index])
-
-            # use current index with allready given curr_sol
-            backtrack(index, cur_sum + candidates[index])
-
-            # delete from the right, last adde dvalue
-            curr_sol.pop()
         
-        backtrack(0,0)
-        return return_list
+        res = []
+
+
+        # backtrack
+        def recursion(i, cur_res):
+            if sum(cur_res) > target or i >= len(candidates):
+                return
+            if sum(cur_res) == target:
+                res.append(cur_res)
+                return
+
             
+            # continnue with curr index
+            recursion(i, cur_res + [candidates[i]])
+
+            # conntinue without curr index
+            recursion(i+1, cur_res)
+        
+        recursion(0,[])
+        
+        return res
+
+
+
 
             
 
